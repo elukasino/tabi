4093115d4ee7f1dd8b6d880c5c94f35f9adba5c0
Lukas Cmiral <cmiralu1@fit.cvut.cz>
2024-06-22 17:29:13 +0200

    Initial Commit

---
+++ myTabi/myTabiApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTabiApp.swift
+//  myTabi
+//
+//  Created by Lukáš Cmíral on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct myTabiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
